a56dd6bfdb955b795609dc4dcb1e967a7c8eb131
Rozhkov Dmitrii <d.rozhkov@entangle.fi>
2024-08-27 19:25:03 +0500

    add chain_id to the config

--- src/solana_logs/config.rs
+++ src/solana_logs/config.rs
@@ -1,5 +1,6 @@
 use serde::{de::Error, Deserialize, Deserializer};
 use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
+use std::str::FromStr;
 
 use crate::solana_transactor::RpcEntry;
 
@@ -9,6 +10,8 @@ pub struct SolanaClientConfig {
     pub commitment: CommitmentConfig,
     pub read_rpcs: Vec<RpcEntry>,
     pub write_rpcs: Vec<RpcEntry>,
+    #[serde(deserialize_with = "deserialize_chain_id")]
+    pub chain_id: u128,
 }
 
 fn deserialize_commitment<'de, D>(deserializer: D) -> Result<CommitmentConfig, D::Error>
@@ -26,4 +29,10 @@ pub struct SolanaListenerConfig {
     pub tx_read_from: String,
     pub tx_read_from_force: Option<String>,
     pub program_listen_to: String,
+}
+
+pub fn deserialize_chain_id<'de, D>(deserializer: D) -> Result<u128, D::Error>
+where D: Deserializer<'de> {
+    let s = String::deserialize(deserializer)?;
+    u128::from_str(&s).map_err(Error::custom)
 }
