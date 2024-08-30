c024c752b47c3b859db888377f093ad7ec2074c2
Rozhkov Dmitrii <d.rozhkov@entangle.fi>
2024-08-30 17:02:35 +0500

    Submodule update

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use solana_sdk::{bs58, signature::Keypair};
 
 pub fn deserialize_keypair<'de, D>(deserializer: D) -> Result<Keypair, D::Error>
 where D: Deserializer<'de> {
-    let s = String::deserialize(deserializer).map_err(|e| D::Error::custom(e))?;
-    let keydata = bs58::decode(s).into_vec().map_err(|e| D::Error::custom(e))?;
-    Keypair::from_bytes(&keydata).map_err(|e| D::Error::custom(e))
+    let s = String::deserialize(deserializer).map_err(D::Error::custom)?;
+    let keydata = bs58::decode(s).into_vec().map_err(D::Error::custom)?;
+    Keypair::from_bytes(&keydata).map_err(D::Error::custom)
 }
