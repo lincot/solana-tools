f71817cccd345bb3dd8e0094aaf21e594e11f6fe
lincot <lincot@disroot.org>
2024-11-20 11:34:28 +0300

    only use optional dependency features directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "anchor-lang-0-29")]
 pub(crate) use anchor_lang_0_29 as anchor_lang;
-#[cfg(feature = "anchor-0-30")]
+#[cfg(feature = "anchor-lang-0-30")]
 pub(crate) use anchor_lang_0_30 as anchor_lang;
 
 pub mod solana_logs;
