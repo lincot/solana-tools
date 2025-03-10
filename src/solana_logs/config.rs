2d78ff03d339455348def487a4413320fbf4d4a7
lincot <lincot@disroot.org>
2025-03-10 14:20:06 +0300

    remove `chain_id` from `SolanaClientConfig`

--- src/solana_logs/config.rs
+++ src/solana_logs/config.rs
@@ -1,6 +1,5 @@
 use serde::{de::Error, Deserialize, Deserializer};
 use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
-use std::str::FromStr;
 
 use crate::solana_transactor::RpcEntry;
 
@@ -10,8 +9,6 @@ pub struct SolanaClientConfig {
     pub commitment: CommitmentConfig,
     pub read_rpcs: Vec<RpcEntry>,
     pub write_rpcs: Vec<RpcEntry>,
-    #[serde(deserialize_with = "deserialize_chain_id")]
-    pub chain_id: u128,
 }
 
 fn deserialize_commitment<'de, D>(deserializer: D) -> Result<CommitmentConfig, D::Error>
@@ -19,10 +16,4 @@ where D: Deserializer<'de> {
     let commitment = CommitmentLevel::deserialize(deserializer)
         .map_err(|err| Error::custom(format!("Malformed commitment: {}", err)))?;
     Ok(CommitmentConfig { commitment })
-}
-
-pub fn deserialize_chain_id<'de, D>(deserializer: D) -> Result<u128, D::Error>
-where D: Deserializer<'de> {
-    let s = String::deserialize(deserializer)?;
-    u128::from_str(&s).map_err(Error::custom)
 }
