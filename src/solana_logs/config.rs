bb3b6ce0b486368fc240c9677c5971ab7b5af7d9
lincot <lincot@disroot.org>
2025-03-10 14:18:12 +0300

    remove SolanaListenerConfig (use SolanaClientConfig and program_listen_to)

--- src/solana_logs/config.rs
+++ src/solana_logs/config.rs
@@ -21,16 +21,6 @@ where D: Deserializer<'de> {
     Ok(CommitmentConfig { commitment })
 }
 
-#[derive(Clone, Debug, Deserialize)]
-pub struct SolanaListenerConfig {
-    #[serde(flatten)]
-    pub client: SolanaClientConfig,
-    #[serde(alias = "txreadfrom")]
-    pub tx_read_from: String,
-    pub tx_read_from_force: Option<String>,
-    pub program_listen_to: String,
-}
-
 pub fn deserialize_chain_id<'de, D>(deserializer: D) -> Result<u128, D::Error>
 where D: Deserializer<'de> {
     let s = String::deserialize(deserializer)?;
