dba3ef2b765ae4dfc83bf471c318ee42acca5dcb
lincot <lincot@disroot.org>
2025-03-17 17:55:15 +0300

    fix event discriminator stripping with anchor v0.31

--- src/solana_logs/parse_logs.rs
+++ src/solana_logs/parse_logs.rs
@@ -100,27 +100,21 @@ fn handle_program_log<T: AnchorDeserialize + Discriminator>(
             }
         };
 
-        let mut slice: &[u8] = &borsh_bytes[..];
-        let disc: [u8; 8] = {
-            let mut disc = [0; 8];
-            disc.copy_from_slice(&borsh_bytes[..8]);
-            slice = &slice[8..];
-            disc
-        };
-        let mut event = None;
-
         #[cfg(feature = "anchor-lang-0-31")]
-        let disc_matches = disc == T::DISCRIMINATOR;
+        let expected_discriminator = T::DISCRIMINATOR;
         #[cfg(not(feature = "anchor-lang-0-31"))]
-        let disc_matches = disc == T::discriminator();
-        if disc_matches {
-            let e: T = AnchorDeserialize::deserialize(&mut slice).map_err(|err| {
-                error!("Failed to deserialize event: {}", err);
-                EventListenerError::SolanaParseLogs
-            })?;
-            event = Some(e);
-        }
-        Ok((event, false))
+        let expected_discriminator = &T::discriminator();
+
+        let Some(event_bytes) = borsh_bytes.strip_prefix(expected_discriminator) else {
+            return Ok((None, false));
+        };
+
+        let event: T = AnchorDeserialize::deserialize(&mut &event_bytes[..]).map_err(|err| {
+            error!("Failed to deserialize event: {}", err);
+            EventListenerError::SolanaParseLogs
+        })?;
+
+        Ok((Some(event), false))
     } else {
         let (_program, did_pop) = handle_irrelevant_log(self_program_str, l);
         Ok((None, did_pop))
