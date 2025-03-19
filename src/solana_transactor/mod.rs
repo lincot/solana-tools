0e5f892d74885cff75ac2ab3990da1d664999cf4
lincot <lincot@disroot.org>
2025-03-19 22:27:11 +0300

    clippy

--- src/solana_transactor/mod.rs
+++ src/solana_transactor/mod.rs
@@ -1,5 +1,3 @@
-#[macro_use]
-
 pub mod alt_manager;
 mod config;
 mod error;
--- src/solana_transactor/round_robin.rs
+++ src/solana_transactor/round_robin.rs
@@ -24,7 +24,7 @@ impl<T> RoundRobin<T> {
         self.pool.get(current_index)
     }
 
-    pub async fn pull_by_max<'a, F>(&'a self, func: F) -> Option<(&T, u64)>
+    pub async fn pull_by_max<'a, F>(&'a self, func: F) -> Option<(&'a T, u64)>
     where F: Fn(&'a T) -> u64 {
         let mut current_max = 0;
         let mut current_index = 0;
