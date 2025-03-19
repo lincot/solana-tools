5f7e4ef920d27491e897915c3cb599c02ba802b3
lincot <lincot@disroot.org>
2025-03-19 22:52:17 +0300

    add heap_frame option to instructions

--- src/solana_transactor/alt_manager.rs
+++ src/solana_transactor/alt_manager.rs
@@ -47,7 +47,7 @@ pub async fn send_with_alt(
         .expect("Failed to get slot");
     let (ix, alt_address) = create_lookup_table(signer.pubkey(), signer.pubkey(), slot);
     debug!("New ALT address {}", alt_address);
-    let ix = InstructionBundle::new(ix, 200000);
+    let ix = InstructionBundle::new(ix, 200000, None);
     transactor
         .send_all_instructions::<&str>(
             None,
@@ -75,7 +75,7 @@ pub async fn send_with_alt(
             is_signer: false,
             is_writable: false,
         });
-        let ix = InstructionBundle::new(ix, 200000);
+        let ix = InstructionBundle::new(ix, 200000, None);
         transactor
             .send_all_instructions::<&str>(
                 None,
@@ -117,7 +117,7 @@ pub async fn send_with_alt(
     }
     debug!("Deactivating ALT");
     let ix = deactivate_lookup_table(alt_address, signer.pubkey());
-    let ix = InstructionBundle::new(ix, 200000);
+    let ix = InstructionBundle::new(ix, 200000, None);
     transactor
         .send_all_instructions::<&str>(
             None,
@@ -134,7 +134,7 @@ pub async fn send_with_alt(
     tokio::time::sleep(Duration::from_secs(240)).await;
     debug!("Clearing ALT");
     let ix = close_lookup_table(alt_address, signer.pubkey(), signer.pubkey());
-    let ix = InstructionBundle::new(ix, 200000);
+    let ix = InstructionBundle::new(ix, 200000, None);
     transactor
         .send_all_instructions::<&str>(
             None,
