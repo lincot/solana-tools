7a4608cc95b5c6590a650d5228e655b09b966893
lincot <lincot@disroot.org>
2025-04-03 22:34:24 +0300

    Revert "ix compiler: only add compute units limit if higher than default"

--- src/solana_transactor/ix_compiler.rs
+++ src/solana_transactor/ix_compiler.rs
@@ -11,7 +11,6 @@ use std::fmt::Display;
 use super::TransactorError;
 use crate::log_with_ctx;
 
-const DEFAULT_CU: u32 = 200_000;
 const MAX_CU: u32 = 1_400_000;
 const MAX_MSG_LEN: usize = 1232 - 65; // assuming only one signature
 
@@ -77,7 +76,7 @@ impl IxCompiler {
         let msg = Message::try_compile(
             &self.payer,
             &[
-                &get_cu_limit_ix(compute_units, 1),
+                &[get_compute_units_ix(compute_units)],
                 &self.get_ix_price_if_any()[..],
                 &get_heap_frame_ix(heap_frame),
                 &[ix.clone()],
@@ -97,7 +96,7 @@ impl IxCompiler {
             heap_frame.map_or(Some(max_heap_frame), |hf| Some(max_heap_frame.max(hf)))
         });
         let ix_buffer = [
-            &get_cu_limit_ix(total_compute_units, self.ix_buffer.len() + 1),
+            &[get_compute_units_ix(total_compute_units)],
             &self.get_ix_price_if_any()[..],
             &get_heap_frame_ix(max_heap_frame),
             &self.ix_buffer[..],
@@ -130,7 +129,7 @@ impl IxCompiler {
             let msg = Message::try_compile(
                 &self.payer,
                 &[
-                    &get_cu_limit_ix(self.total_compute_units, self.ix_buffer.len()),
+                    &[get_compute_units_ix(self.total_compute_units)],
                     &self.get_ix_price_if_any()[..],
                     &get_heap_frame_ix(self.max_heap_frame),
                     &self.ix_buffer[..],
@@ -168,7 +167,7 @@ impl IxCompiler {
         let msg = Message::try_compile(
             &self.payer,
             &[
-                &get_cu_limit_ix(self.total_compute_units, self.ix_buffer.len()),
+                &[get_compute_units_ix(self.total_compute_units)],
                 &self.get_ix_price_if_any()[..],
                 &get_heap_frame_ix(self.max_heap_frame),
                 &self.ix_buffer[..],
@@ -195,20 +194,8 @@ fn approaches_limits(msg_len: usize, compute_units: u32) -> bool {
     msg_len >= MAX_MSG_LEN - 32 || compute_units >= MAX_CU - 200_000
 }
 
-/// Returns a vector containing the CU (compute unit) limit instruction if the
-/// requested CU exceed the default allocation, otherwise returns an empty
-/// vector.
-///
-/// The default CU allocation is calculated based on the number of the top-level
-/// instructions that are not invocations of the Compute Budget program
-/// (`regular_ix_count`).
-fn get_cu_limit_ix(compute_units: u32, regular_ix_count: usize) -> Vec<Instruction> {
-    if compute_units <= DEFAULT_CU * regular_ix_count as u32 {
-        return Vec::new();
-    }
-    vec![ComputeBudgetInstruction::set_compute_unit_limit(
-        compute_units,
-    )]
+fn get_compute_units_ix(compute_units: u32) -> Instruction {
+    ComputeBudgetInstruction::set_compute_unit_limit(compute_units)
 }
 
 fn get_heap_frame_ix(heap_frame: Option<u32>) -> Vec<Instruction> {
