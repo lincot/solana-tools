ef2deba6004ed0570a687a9869316839e356e3e2
lincot <lincot@disroot.org>
2025-05-12 20:31:21 +0300

    Revert "add payer to ALT in `send_with_alt`"
    This reverts commit c73ac6174616ff2479c719bcf938d3664d7dec64.

--- src/solana_transactor/alt_manager.rs
+++ src/solana_transactor/alt_manager.rs
@@ -36,14 +36,7 @@ pub async fn send_with_alt(
         .collect();
     let total_addresses: Vec<_> = instructions
         .iter()
-        .flat_map(|x| {
-            x.instruction
-                .accounts
-                .clone()
-                .into_iter()
-                .map(|x| x.pubkey)
-                .chain(std::iter::once(signer.pubkey()))
-        })
+        .flat_map(|x| x.instruction.accounts.clone().into_iter().map(|x| x.pubkey))
         .collect();
     let to_add: HashSet<_> =
         total_addresses.iter().cloned().filter(|x| !alt_addresses.contains(x)).collect();
